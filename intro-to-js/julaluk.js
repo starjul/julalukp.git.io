200e76cea240354952fb793b33c93adf1d44fad2
Julaluk Paladkhwa <j.paladkhwa@gmail.com>
2022-02-18 14:18:38 +0700

    [New Assignment]

--- intro-to-js/julaluk.js
+++ intro-to-js/julaluk.js
@@ -9,3 +9,16 @@ function askMyName() {
     const h1message = document.getElementById("my-name").innerHTML
     alert(h1message + " อย่าไปหยอง เพราะเราไม่ใช่หมู")
  }
+
+ function calAge() {
+    let yearofBirth = prompt("Please enter your year of birth");
+    yourAge = 2022 - Number(yearofBirth); 
+    alert(`ยินดีด้วย, นายอยู่ในประเทศเก๊กฮวยนี้มา ${yourAge} ปีแล้ว`)
+ }
+
+ function loadProfile() {
+   myName = "Julaluk" ;
+   myProvince = "Khon Kaen" ;
+   document.getElementById("fname").innerHTML = myName ;
+   document.getElementById("my-province").innerHTML = myProvince;
+}
